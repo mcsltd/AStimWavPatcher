1c0aceb224d67777dee309ca3aea396bedf6dbb3
Kosachev Daniil <kos-dan@mail.ru>
2021-07-21 15:44:13 +0300

    Added errors handling

--- wavpatcher.py
+++ wavpatcher.py
@@ -1,11 +1,33 @@
+import os
 import argparse
+import traceback
+
+
+class Error(Exception):
+    def __init__(self, message):
+        super(Error, self).__init__(message)
 
 
 def main():
-    input_file, output_file = _parse_args()
-    channels, params = _read_file(input_file)
-    channels = _add_trigger(channels)
-    _write_file(channels, params, output_file)
+    try:
+        input_file, output_file = _parse_args()
+        channels, params = _read_file(input_file)
+        channels = _add_trigger(channels)
+        _write_file(channels, params, output_file)
+    except Error as exc:
+        print("Error: {0}\n".format(exc))
+    except Exception as exc:
+        if _is_debug():
+            raise
+        log_filename = "errors-log.txt"
+        message = "Fatal error! {0}: {1}. See details in file '{2}'."
+        print(message.format(type(exc).__name__, exc, log_filename))
+        with open(log_filename, "wt") as log:
+            log.write(traceback.format_exc())
+
+
+def _is_debug():
+    return getattr(os.sys, 'gettrace', None) is not None
 
 
 def _parse_args():
