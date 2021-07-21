b016bedcd58ad3c96c845e1a865be8c65ac5bad6
Kosachev Daniil <kos-dan@mail.ru>
2021-07-21 15:41:32 +0300

    Added '_parse_args'

--- wavpatcher.py
+++ wavpatcher.py
@@ -1,8 +1,21 @@
+import argparse
+
+
 def main():
     input_file, output_file = _parse_args()
     channels, params = _read_file(input_file)
     channels = _add_trigger(channels)
     _write_file(channels, params, output_file)
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description="Patch .wav files")
+    parser.add_argument("input_file", help="Path to input .wav file")
+    parser.add_argument(
+        "output_file", help="Path to output .wav file", required=False)
+    data = parser.parse_args()
+    output_file = data.output_file or data.input_file
+    return data.input_file, output_file
 
 
 if __name__ == "__main__":
